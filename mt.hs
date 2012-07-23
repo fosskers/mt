6bd2db34fabb4fc76e1c5c6f68580b2193a4f49f
Colin Woodbury <colingw@gmail.com>
2012-07-23 21:13:47 +0900

    Cleaned up source
    - Version bump.

--- mt.hs
+++ mt.hs
@@ -1,4 +1,6 @@
--- Printing a multiplication table.
+-- mt - Prints a multiplication table.
+-- Written by Colin Woodbury <colingw@gmail.com>
+-- Usage: mt <number>
 
 import System.Environment (getArgs)
 import Text.Printf (printf)
@@ -17,17 +19,17 @@ processArgs (n:_) | and $ map isDigit n = read n
                   | otherwise = 10
 
 makeTable :: Int -> String
-makeTable lim = topRow ++ "\n" ++ underline ++ "\n" ++                
-                (concat $ map (\m -> renderRow (show m) m lim ++ "\n") [1..lim])
-    where topRow    = renderRow "" 1 lim
-          underline = replicate (read pad + 2 + (lim * (read pad + 1))) '-'
-          pad       = getPad lim
+makeTable lim = topRow ++ "\n" ++ border ++ "\n" ++ table
+  where topRow = renderRow "" 1 lim
+        border = replicate (read pad + 2 + (lim * (read pad + 1))) '-'
+        table  = concat $ map (\m -> renderRow (show m) m lim ++ "\n") [1..lim]
+        pad    = getPad lim
 
 renderRow :: String -> Int -> Int -> String
 renderRow label mult lim = printf ("%" ++ pad ++ "s |%s") label renderedRow
-    where renderedRow = concat $ map renderNum [1..lim]
-          renderNum n = printf (" %" ++ pad ++ "d") (n * mult) :: String
-          pad         = getPad lim
+  where renderedRow = concat $ map renderNum [1..lim]
+        renderNum n = printf (" %" ++ pad ++ "d") (n * mult) :: String
+        pad         = getPad lim
 
 getPad :: Int -> String
 getPad lim = show . length . show $ lim * lim
